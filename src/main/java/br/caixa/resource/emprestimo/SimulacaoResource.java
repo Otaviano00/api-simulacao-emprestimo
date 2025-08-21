7d8dc578e5f40e9bb3fe62a603a024b0d155bbbc
Kauã Otaviano <kauaotaviano100@gmail.com>
2025-08-21 09:54:56 -0300

    feat: configuração inicial do swagger

--- src/main/java/br/caixa/resource/emprestimo/SimulacaoResource.java
+++ src/main/java/br/caixa/resource/emprestimo/SimulacaoResource.java
@@ -1,6 +1,7 @@
 package br.caixa.resource.emprestimo;
 
 import br.caixa.dto.emprestimo.simulacao.SimulacaoEmprestimoRequest;
+import br.caixa.entity.Produto;
 import br.caixa.service.ProdutoService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -9,6 +10,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 @Path("api/v0/emprestimo/simulacao")
 @ApplicationScoped
@@ -25,6 +31,15 @@ public class SimulacaoResource {
     }
 
     @Path("produto")
+    @Operation(
+            summary = "Listar Produtos simulados",
+            description = "Retorna uma lista com valores simulados para cada produto"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Simulação realizada com sucesso",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Produto.class, type = SchemaType.ARRAY))
+    )
     @GET
     public Response getSimulacoes() {
         var produtos = produtoService.getAllProdutos();
